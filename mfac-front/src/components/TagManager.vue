0f1bf1f5ced9c04705a207dd0fda7ad8933204c3
FreshStuptMan <1367213168@qq.com>
2024-07-06 19:11:19 +0800

    完成了标签页面的CRUD 并且将分页改为了饿了么ui的组件

--- mfac-front/src/components/TagManager.vue
+++ mfac-front/src/components/TagManager.vue
@@ -75,9 +75,9 @@
                     </template>
                 </vs-table>
                 <!-- 分页 -->
-                <vs-pagination style="margin-top: 20px;" prev-icon="fa-angle-double-left"
-                    next-icon="fa-angle-double-right" icon-pack="fa" :total="30" :max="pageSize" v-model="pageNum">
-                </vs-pagination>
+                <el-pagination style="float: right;margin-top: 20px;" background layout="prev, pager, next" @current-change="handlePageChange"
+                    :page-size="pageSize" :current-page="pageNum" :total="total">
+                </el-pagination>
             </div>
             <!-- 创建表单弹窗 -->
             <vs-popup title="创建标签" :active.sync="craetePopVis" :button-close-hidden="true">
@@ -109,15 +109,27 @@
                     <vs-button @click="cancelEdit" style="float: right;" color="danger" type="line">取消</vs-button>
                 </div>
             </vs-popup>
+            <!-- 删除二次确认弹窗 -->
+            <vs-popup
+                title="提示" :active.sync="deletePopVis">
+                <p>
+                    确定要删除吗？
+                </p>
+                <div style="float: right;">
+                    <vs-button @click="acceptDelete" style="margin-right: 10px;" color="danger" type="filled">确定</vs-button>
+                    <vs-button @click="cancalDelete" color="warning" type="filled">取消</vs-button>
+                </div>
+            </vs-popup>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    watch: {
-        pageNum(newValue, oldValue) {
-            console.log("进行分页")
+    computed: {
+        token() {
+            return this.$store.state.user.userInfo.token ? this.$store.state.user.userInfo.token : ''
         }
     },
     data() {
@@ -126,12 +138,6 @@ export default {
             SearchForm: {
                 name: '',
             },
-            // Select测试数据
-            options: [
-                { text: 'IT', value: 0 },
-                { text: 'Blade Runner', value: 2 },
-                { text: 'Thor Ragnarok', value: 3 },
-            ],
             // 表格测试数据
             tags: [{
                 id: 1,
@@ -155,39 +161,159 @@ export default {
             },
             // 编辑表单窗口控制
             editPopVis: false,
+            // 被选中的标签
+            activeTag: {},
+            // 删除的二次确认弹窗控制
+            deletePopVis: false,
             // 分页参数
             pageNum: 1,
-            pageSize: 8
+            pageSize: 8,
+            total: 0
         }
     },
     methods: {
+        handlePageChange(pageNum) {
+            this.pageNum = pageNum
+            this.GetTagList()
+        },
         // 搜索
         handleSearch() {
-            console.log("搜索博客")
+            this.pageNum = 1
+            this.GetTagList()
         },
         // 重置搜索表单
         handleReset() {
-            console.log("重置搜索表单")
+            this.SearchForm.name = ''
+            this.pageNum = 1
+            this.GetTagList()
         },
+        // 获取标签列表
+        GetTagList() {
+            axios.post('/api/admin/tag/list',{
+                pageNum: this.pageNum,
+                pageSize: this.pageSize,
+                name: this.SearchForm.name
+            },{
+                headers: {token: this.token}
+            })
+            .then(response => {
+                if(response.data.code === 200) {
+                    this.tags = response.data.data.records
+                    this.total = response.data.data.total
+                    console.log(this.total)
+                } else {
+                    this.$vs.notify({
+                        title: '提示',
+                        text: response.data.msg,
+                        color: 'red'
+                    })
+                }
+            })
+            .catch(error => {
+                this.$vs.notify({
+                    title: '提示',
+                    text: error,
+                    color: 'red'
+                })
+            })
+        },
+
+
+
         // 编辑标签
         handleEdit(tag) {
-            console.log("编辑标签")
+            this.activeTag = tag
+            this.editForm.name = tag.name
+            this.editForm.color = tag.color
             this.editPopVis = true
         },
         // 确定编辑
         acceptEdit() {
-            console.log("确定编辑")
-            this.editPopVis = false
+            if(!this.editFormVarify()) {
+                return
+            }
+            axios.post('/api/admin/tag/update', {
+                id: this.activeTag.id,
+                name: this.editForm.name,
+                color: this.editForm.color
+            }, {
+                headers: { token: this.token }
+            })
+            .then(response => {
+                if(response.data.code === 200) {
+                    this.$vs.notify({
+                        title: '提示',
+                        text: '标签更新成功',
+                        color: 'success'
+                    })
+                    this.GetTagList()
+                    this.editPopVis = false
+                } else {
+                    this.$vs.notify({
+                        title: '提示',
+                        text: response.data.msg,
+                        color: 'red'
+                    })
+                }
+            })
+            .catch(error => {
+                this.$vs.notify({
+                    title: '提示',
+                    text: error,
+                    color: 'red'
+                })
+            })
         },
         // 取消编辑
         cancelEdit() {
-            console.log("取消编辑")
             this.editPopVis = false
         },
+
+
+
         // 删除标签
         handleDelete(tag) {
-            console.log("删除标签")
+            this.activeTag = tag
+            this.deletePopVis = true
         },
+        // 确定删除
+        acceptDelete() {
+            axios.delete(`/api/admin/tag/delete/${this.activeTag.id}`, {
+                headers: { token: this.token }
+            })
+            .then(response => {
+                if(response.data.code === 200) {
+                    this.$vs.notify({
+                        title: '提示',
+                        text: '删除成功',
+                        color: 'success'
+                    })
+                    this.GetTagList()
+                } else {
+                    this.$vs.notify({
+                        title: '提示',
+                        text: response.data.msg,
+                        color: 'red'
+                    })
+                }
+            })
+            .catch(error => {
+                this.$vs.notify({
+                    title: '提示',
+                    text: error,
+                    color: 'red'
+                })
+            })
+            this.cancalDelete()
+        },
+        // 取消删除
+        cancalDelete() {
+            this.activeTag = {}
+            this.deletePopVis = false
+        },
+
+
+
         // 创建标签
         handleCreate() {
             this.createFrom.name = '标签名称'
@@ -196,14 +322,90 @@ export default {
         },
         // 确认创建
         acceptCreate() {
-            console.log("确认创建")
-            this.craetePopVis = false
+            if(!this.createFormVarify()) {
+                return
+            }
+            axios.post('/api/admin/tag/create', this.createFrom, {
+                headers: {
+                    token: this.token
+                }
+            })
+            .then(response => {
+                if(response.data.code === 200) {
+                    this.$vs.notify({
+                        title: '提示',
+                        text: '创建成功',
+                        color: 'success'
+                    })
+                    this.craetePopVis = false
+                    this.GetTagList()
+                } else {
+                    this.$vs.notify({
+                        title: '提示',
+                        text: response.data.msg,
+                        color: 'red'
+                    })
+                }
+            })
+            .catch(error => {
+                this.$vs.notify({
+                    title: '提示',
+                    text: error,
+                    color: 'red'
+                })
+            })
         },
         // 取消创建
         cancelCreate() {
             console.log("取消创建")
             this.craetePopVis = false
+        },
+
+
+
+        // 标签创建表单验证
+        createFormVarify() {
+            if(this.createFrom.name === '标签名称' || '') {
+                this.$vs.notify({
+                    title: '提示',
+                    text: '请填写完整的标签信息',
+                    color: 'red'
+                })
+                return false
+            }
+            if(this.createFrom.color === '') {
+                this.$vs.notify({
+                    title: '提示',
+                    text: '请选择标签的颜色',
+                    color: 'yellow'
+                })
+                return false
+            }
+            return true
+        },
+        // 标签编辑表单验证
+        editFormVarify() {
+            if(this.editForm.name === this.activeTag.name && this.editForm.color === this.activeTag.color) {
+                this.$vs.notify({
+                    title: '提示',
+                    text: '请做出修改后再提交',
+                    color: 'red'
+                })
+                return false
+            }
+            if(this.editForm.name === '' || this.editForm.color === '') {
+                this.$vs.notify({
+                    title: '提示',
+                    text: '请填写完整的标签信息',
+                    color: 'red'
+                })
+                return false
+            }
+            return true
         }
+    },
+    mounted() {
+        this.GetTagList()
     }
 }
 </script>
